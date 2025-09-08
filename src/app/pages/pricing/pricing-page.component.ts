d52622bd76e7aac487ade8260e1b257c45c8b45e
Dolorv86 <dolorv86@gmail.com>
2025-09-08 20:50:03 +0200

    Uso de PLATFORM_ID - cliente y server

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -11,8 +12,14 @@ export default class PricingPageComponent implements OnInit {
 
   private title = inject(Title);
   private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+
+    //if(!isPlatformServer(this.platform)){
+    //  document.title = 'Pricing Page';
+    //}
+
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({ name: 'description', content: 'Pricing Page Description' });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
