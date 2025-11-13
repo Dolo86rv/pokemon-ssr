2a5fbaa17e702cac14fa8d2ce9375bfb5b820cad
Dolorv86 <dolorv86@gmail.com>
2025-11-13 12:08:45 +0100

    Configurando rutas para prerendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,30 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/page/:page',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Genera las primeras 5 páginas (puedes ajustar según tus necesidades)
+      const pages = [];
+      for (let i = 1; i <= 5; i++) {
+        pages.push({ page: i.toString() });
+      }
+      return pages;
+    }
+  },
+  {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Genera los primeros 20 pokémons (puedes ajustar según tus necesidades)
+      const pokemons = [];
+      for (let i = 1; i <= 20; i++) {
+        pokemons.push({ id: i.toString() });
+      }
+      return pokemons;
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
